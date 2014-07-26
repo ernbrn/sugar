a5306527dde9baaad280b18976a405952ce4b063
Andrew Hooge <andrewhooge@pushactive.com>
2014-07-26 17:33:46 -0400

    added configuration to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require underscore
+//= require backbone
+//= require app
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
