e6de1e6988ac5fe83cc3ae4158dcab033fd445a0
Erin Brown <emberbrown@gmail.com>
2014-07-26 10:07:12 -0400

    Add base api for student model

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,19 +1,31 @@
 class StudentsController < ApplicationController
   respond_to :json
-  before_action :find_student, only: [:show]
+  before_action :find_student, only: [:show, :update, :destroy]
 
 
   def index
     @students = Student.all
-    respond_with @students
+    respond_with @students, only: [:name, :email, :bio, :github, :linkdin]
   end
 
   def show
-    respond_with @student
+    respond_with @student, only: [:name, :email, :bio, :github, :linkdin]
   end
 
   def create
+    @student = Student.new(student_params)
+    @student.save
+    respond_with @student
+  end
 
+  def update
+    @student.update(student_params)
+    respond_with @student
+  end
+
+  def destroy
+    @student.destroy
+    respond_with @student
   end
 
   private
--- spec/controllers/students_spec.rb
+++ spec/controllers/students_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 describe StudentsController do
-  it 'Get :index' do
+
+  it 'GET :index' do
     student = create(:student)
     get :index, format: :json
     expect(response).to be_success
@@ -23,44 +24,41 @@ describe StudentsController do
       expect(data["name"]).to eq student.name
     end
 
-    #
-    # it "serve image_url instead of image_uid" do
-    #
-    # end
 
   end
-  #
-  # describe 'POST :create' do
-  #   it 'succeeds when all attributes are set' do
-  #
-  #   end
-  #
-  #   it 'POST :create with Error' do
-  #
-  #   end
-  #
-  #   it 'cannot create with the wrong format' do
-  #
-  #   end
-  # end
-  #
-  # describe "PATCH :update" do
-  #   it 'succeeds when data is changed' do
-  #
-  #   end
-  #
-  #   it 'fails when a required field is missing' do
-  #
-  #   end
-  #
-  # end
-  #
-  # describe 'DELETE :destroy' do
-  #   it 'can delete an existing record' do
-  #
-  #   end
-  #
-  #   it 'cannot delete a nonexistent record' do
-  #   end
-  # end
+
+  describe 'POST :create' do
+    before { @student_attributes = attributes_for(:student)}
+    it 'succeeds when all attributes are set' do
+      post :create, student: @student_attributes, format: :json
+      expect(response).to have_http_status(:created)
+      data = JSON.parse(response.body)
+      expect(data).to have_key('name')
+      expect(data["name"]).to eq @student_attributes[:name]
+      expect(data).to have_key('authentication_token')
+    end
+
+  end
+
+  describe "PATCH :update" do
+    before { @student = create(:student) }
+    it 'succeeds when data is changed' do
+     patch :update, id: @student.id, student:{name: "Henry"}, format: :json
+     expect(response).to have_http_status(:no_content)
+     expect(Student.find(@student.id).name).to eq 'Henry'
+    end
+
+
+  end
+
+
+  describe 'DELETE :destroy' do
+    it 'can delete an existing record' do
+      student = create(:student)
+      delete :destroy, id: student.id, format: :json
+      expect(response).to have_http_status(:no_content)
+      expect{Student.find(student.id)}.to raise_error ActiveRecord::RecordNotFound
+    end
+
+  end
 end
