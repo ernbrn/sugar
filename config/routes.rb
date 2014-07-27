23978da4e7695720f382d23e4979fdfd68e07f13
Dave Seidman <bullcitydave@gmail.com>
2014-07-27 17:21:48 -0400

    Main page with list already displaying..this will replaced by login page.

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  home 'welcome#home'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
