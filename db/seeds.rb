88a69725bb477b85c163819becd1f9350d120fd7
Erin Brown <emberbrown@gmail.com>
2014-07-28 17:10:38 -0400

    fixed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,15 +17,7 @@ Student.create(
     admin: true
     )
 
-Student.create(
-name:"Erin Brown",
-email: "emberbrown@gmail.com",
-password:"12345678",
-cohort: ror,
-twitter: "@ernbrn",
-linkedin: "https://www.linkedin.com",
-github:"ernbrn",
-bio:"Erin is a local Durham musician. She’s played all over the Triangle and beyond. You may have seen her musical talents on
+Student.create(name:"Erin Brown", email: "emberbrown@gmail.com",password:"12345678",cohort: ror,twitter: "@ernbrn",linkedin: "https://www.linkedin.com",github:"ernbrn",bio:"Erin is a local Durham musician. She’s played all over the Triangle and beyond. You may have seen her musical talents on
 display at Fullsteam, Parker and Otis, Pinhook, the Cave, Open Eye Cafe, Carrborro Music Fest among others.
 She keeps a running blog with her music and life (including her experience at The Iron Yard) which you can
 check out at www.ern-brn.com. Erin’s also talented at making up words and she’s competed in two of Durham's
@@ -85,14 +77,7 @@ her professional energy.")
 
 
 Student.create(
-    name:"Mae Beale",
-    email: "emberbrown@gmail.com",
-    password:"12345678",
-    cohort: ror,
-    twitter: "@ernbrn",
-    linkedin: "https://www.linkedin.com",
-    github:"ernbrn",
-    bio:"Erin is a local Durham musician. She’s played all over the Triangle and beyond. You may have seen her musical talents on
+name:"Mae Beale", email: "mae@mae.com", password:"12345678", cohort: ror, twitter: "@ernbrn", linkedin: "https://www.linkedin.com", github:"ernbrn", bio:"Erin is a local Durham musician. She’s played all over the Triangle and beyond. You may have seen her musical talents on
 display at Fullsteam, Parker and Otis, Pinhook, the Cave, Open Eye Cafe, Carrborro Music Fest among others.
 She keeps a running blog with her music and life (including her experience at The Iron Yard) which you can
 check out at www.ern-brn.com. Erin’s also talented at making up words and she’s competed in two of Durham's
@@ -103,7 +88,7 @@ back-end engineer.")
 
 Student.create(
     name:"Sebastian",
-    email: "andrewhooge@pushactive.com",
+    email: "seb@seb.com",
     password:"12345678",
     cohort: ror,
     twitter: "@andrewhooge",
@@ -121,7 +106,7 @@ a passion for improving health outcomes with technology.")
 
 Student.create(
     name:"Emily Bond",
-    email: "bullcitydave@gmail.com",
+    email: "emily@em.com",
     password:"12345678",
     cohort: ror,
     twitter: "@bullcitydave",
@@ -139,7 +124,7 @@ appeared on one of the very first airings of Who Wants To Be A Millionaire. Phot
 
 Student.create(
     name:"Chet Corey",
-    email: "amygori@gmail.com",
+    email: "chet@chetcorey.com",
     password:"12345678",
     cohort: fe,
     twitter: "@amygori",
