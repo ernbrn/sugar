61c8be23045e170e999ff19623183ecd286eef03
Erin Brown <emberbrown@gmail.com>
2014-07-27 21:08:34 -0400

    Revert "Revert "Backbone and hello""

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,54 @@
+var HomeView = Backbone.View.extend ({
+	className : 'home',
+		initialize:function(){
+			console.log('Welcome to Sugar!');
+		},
+
+		render: function () {
+
+				var source = $('#home-template').html();
+				this.$el.html(source);
+				console.log('Home page rendered');
+				return this;
+			},
+
+		events: {
+			// 'click body': 'hoverItem',
+			// 'click .container' : 'hoverItem',
+		'hover .vine-selecton-entry' : 'hoverItem'
+
+		},
+
+
+
+		hoverItem: function (e) {
+			alert('Clicked');
+				e.currentTarget.css({"border-color" : "#FFF"});
+		}
+
+
+});
+
+
+
+// Launch home page
+
+
+$(function () {
+	'use strict';
+	var homeView = new HomeView();
+
+		$('.container').load($('.home-template').html());
+
+
+});
+
+
+  $('div').click(function(e) {
+       $(event.currentTarget).child('img').css('border-color','#fff');
+});
+
+$('h1').click(function(e) {
+		$('img').css('border-color','white');
+		alert('Hello');
+});
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,7 @@
 //= require jquery_ujs
 //= require underscore
 //= require backbone
+//= require models
+//= require views
+//= require router
+//= require app
